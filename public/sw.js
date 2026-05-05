2460f99b738e88c252f148c55bf3ef8b2cf27a31
verdesotostiven4-commits <verdesotostiven4@gmail.com>
2026-05-05 02:48:26 -0500

    Updated tailwind.config.js

--- public/sw.js
+++ public/sw.js
@@ -1,29 +1,20 @@
-const CACHE = 'pollazo-v2';
-const ASSETS = ['/', '/manifest.json', '/logo-final.png'];
+const CACHE = 'pollazo-v1';
+const PRECACHE = ['/', '/index.html', '/logo-pollazo.png', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
-  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(PRECACHE)).then(() => self.skipWaiting()));
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
   e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone)).catch(() => {});
-        return res;
-      })
-      .catch(() => caches.match(e.request))
+    caches.match(e.request).then(cached => cached || fetch(e.request))
   );
 });
